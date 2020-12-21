64c3e838ed215dce95171d0a9e0ed503b640df3c
Sukhvsin2 <sukhvsingh2026@gmail.com>
2020-12-21 12:28:24 +0530

    Connection error

--- src/pizzaBackend/asgi.py
+++ src/pizzaBackend/asgi.py
@@ -1,8 +1,6 @@
 """
-ASGI config for pizzaBackend project.
-
+ASGI config for pizza project.
 It exposes the ASGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
@@ -10,7 +8,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 import os
 from channels.routing import ProtocolTypeRouter,URLRouter
 from channels.auth import AuthMiddlewareStack
-from django.urls import path, re_path
+from django.urls import path
 from django.core.asgi import get_asgi_application
 from home import consumers
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pizza.settings')
@@ -18,7 +16,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pizza.settings')
 application = get_asgi_application()
 
 ws_pattern= [
-    path('ws/pizza/<order_id>',consumers.OrderProgress),
+    path('ws/pizza/<order_id>', consumers.OrderProgress),
 ]
 
 application= ProtocolTypeRouter(
