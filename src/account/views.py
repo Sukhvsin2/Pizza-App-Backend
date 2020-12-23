2ae24213e3b3d6f9abf99538b2f8a25d61b936e2
Sukhvsin2 <sukhvsingh2026@gmail.com>
2020-12-23 18:37:10 +0530

    Token Error Solved

--- src/account/views.py
+++ src/account/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from django.contrib.auth.models import User, update_last_login
 from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
+import json
 
 class RegisterView(APIView):
 
@@ -16,7 +17,7 @@ class RegisterView(APIView):
         # user.first_name(firstname)
         # user.last_name(lastname)
         user.set_password(password)
-        refresh = RefreshToken.for_user(user)
+        # refresh = RefreshToken.for_user(user)
         user.save()
         return Response({'message': 'User Created'}, status=status.HTTP_200_OK)
 
@@ -30,7 +31,9 @@ class LoginView(APIView):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 update_last_login(None, user)
-                return Response({'token': 'user'},status=status.HTTP_200_OK)
+                token = RefreshToken.for_user(user)
+                print(token)
+                return Response({'token': str(token)},status=status.HTTP_200_OK)
             else:
                 return Response({'message': 'username or password incorrect'}, status=status.HTTP_404_NOT_FOUND)
         else:
