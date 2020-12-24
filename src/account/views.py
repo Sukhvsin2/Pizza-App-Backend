8834aa1825498b4c2e6afb3a7baaf02d23042038
Sukhvsin2 <sukhvsingh2026@gmail.com>
2020-12-24 18:25:10 +0530

    Token Issue

--- src/account/views.py
+++ src/account/views.py
@@ -4,20 +4,17 @@ from rest_framework.response import Response
 from django.contrib.auth.models import User, update_last_login
 from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
-import json
+from json import encoder
+from .models import MyUserToken
 
 class RegisterView(APIView):
 
     def post(self, request):
         username = request.data['username']
         password = request.data['password']
-        # firstname = request.data['firstname']
-        # lastname = request.data['lastname']
         user = User(username=username)
-        # user.first_name(firstname)
-        # user.last_name(lastname)
         user.set_password(password)
-        # refresh = RefreshToken.for_user(user)
+        refresh = RefreshToken.for_user(user)
         user.save()
         return Response({'message': 'User Created'}, status=status.HTTP_200_OK)
 
@@ -31,9 +28,9 @@ class LoginView(APIView):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 update_last_login(None, user)
-                token = RefreshToken.for_user(user)
-                print(token)
-                return Response({'token': str(token)},status=status.HTTP_200_OK)
+                refresh = RefreshToken.for_user(user)
+                print(refresh)
+                return Response({'token': str(refresh)},status=status.HTTP_200_OK)
             else:
                 return Response({'message': 'username or password incorrect'}, status=status.HTTP_404_NOT_FOUND)
         else:
